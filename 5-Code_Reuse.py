85ceedfe11ff43ae6fd3a6033578a8055e896850
ebtrader <jsiddique@gmail.com>
2020-12-13 17:09:12 -0500

    Commit

---
+++ 5-Code_Reuse.py
@@ -0,0 +1,22 @@
+# OOP tutorial
+# 5 Code Reuse
+# https://towardsdatascience.com/understand-o-o-p-in-python-with-one-article-bfa76f3ba48c
+
+class MyTransports():
+    def __init__(self, color, brand):
+        self.color = color
+        self.brand = brand
+
+class Car(MyTransports):
+    pass
+
+class Train(MyTransports):
+    pass
+
+my_train = Train('gray', 'Tesla')
+
+print (my_train.brand)
+
+my_car = Car('black', 'Lambo')
+
+print(my_car.brand)
