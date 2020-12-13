fb5db642de3cc38490dc08864d4e44fa331f4236
ebtrader <jsiddique@gmail.com>
2020-12-13 18:14:02 -0500

    Commit

---
+++ Real_Python/Define_a_Class.py
@@ -0,0 +1,6 @@
+# https://realpython.com/python3-object-oriented-programming/
+
+kirk = ["James Kirk", 34, "Captain", 2265]
+spock = ["Spock", 35, "Science Officer", 2254]
+mccoy = ["Leonard McCoy", "Chief Medical Officer", 2266]
+
