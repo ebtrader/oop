dfdaabdd9eb71feea997dbb483aba5f3a5b41671
ebtrader <jsiddique@gmail.com>
2020-12-14 16:50:54 -0500

    Commit

---
+++ Real_Python/Class and Instance Attributes.py
@@ -0,0 +1,24 @@
+class Dog:
+    species = 'Canis familiaris'
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+buddy = Dog('Buddy', 9)
+miles = Dog('Miles', 4)
+
+print (buddy.name)
+
+print (buddy.age)
+
+print (miles.name)
+print (miles.age)
+
+print (buddy.species)
+
+buddy.age = 10
+
+print(buddy.age)
+
+miles.species = 'Felis silvestris'
+print (miles.species)
--- Real_Python/Define_a_Class.py
+++ Real_Python/Define_a_Class.py
@@ -4,3 +4,6 @@ kirk = ["James Kirk", 34, "Captain", 2265]
 spock = ["Spock", 35, "Science Officer", 2254]
 mccoy = ["Leonard McCoy", "Chief Medical Officer", 2266]
 
+class Dog:
+    pass
+
