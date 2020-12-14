4003d85526def45dbf60a628a8bfaca1adefce83
ebtrader <jsiddique@gmail.com>
2020-12-14 17:05:46 -0500

    Commit

---
+++ Real_Python/Instance Methods.py
@@ -0,0 +1,34 @@
+# https://realpython.com/python3-object-oriented-programming/
+
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Instance method
+    def description(self):
+        return f"{self.name} is {self.age} years old"
+
+    # Another instance method
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+miles = Dog("Miles", 4)
+
+print(miles.description())
+
+print(miles.speak('Woof Woof'))
+
+print(miles.speak('Bow Wow'))
+
+names = ['Fletcher', 'David', 'Dan']
+print(names)
+
+print(miles)
+
+
+
+
+
